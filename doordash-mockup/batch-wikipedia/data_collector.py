8e50f129c8a7f6f376bdea2caaf0a6e71579029b
Daniel Tanaka <daniel.tanaka@poatek.com>
2023-10-16 10:42:07 -0300

    Removed secrets

--- doordash-mockup/batch-wikipedia/data_collector.py
+++ doordash-mockup/batch-wikipedia/data_collector.py
@@ -4,9 +4,6 @@ import json
 import boto3
 import os
 
-os.environ['AWS_ACCESS_KEY'] = "AKIAZNAPYPLCYKAKLD4H"
-os.environ['AWS_SECRET_ACCESS'] = "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"
-
 aws_access_key_id = os.getenv("AWS_ACCESS_KEY")
 aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS")
 aws_bucket='doordash-mockup-daniel'
